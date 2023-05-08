aa60411a487620453ebb52c360d3e8eb466328aa
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2023-05-08 01:48:19 +0100

    new buttons
    Created new buttons and added them toa nice layout =)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -38,12 +38,45 @@ MainWindow::MainWindow(QWidget *parent)
     helloLabel->setStyleSheet("font-size: 22px; color: white;");
     gridLayout->addWidget(helloLabel, 0, 0, Qt::AlignTop | Qt::AlignHCenter);
 
-    //home button
+    // create the buttons
     QPushButton *homeButton = new QPushButton(mainWidget);
     homeButton->setIcon(QIcon(":/imgs/logoHome.png"));
     homeButton->setIconSize(QSize(64, 64));
+    homeButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QPushButton *goalsButton = new QPushButton(mainWidget);
+    goalsButton->setIcon(QIcon(":/imgs/logoGoals.png"));
+    goalsButton->setIconSize(QSize(64, 64));
+    goalsButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QPushButton *habitsButton = new QPushButton(mainWidget);
+    habitsButton->setIcon(QIcon(":/imgs/logoHabits.png"));
+    habitsButton->setIconSize(QSize(64, 64));
+    habitsButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QPushButton *inspirationButton = new QPushButton(mainWidget);
+    inspirationButton->setIcon(QIcon(":/imgs/logoInspiration.png"));
+    inspirationButton->setIconSize(QSize(64, 64));
+    inspirationButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    QPushButton *notesButton = new QPushButton(mainWidget);
+    notesButton->setIcon(QIcon(":/imgs/logoNotes.png"));
+    notesButton->setIconSize(QSize(64, 64));
+    notesButton->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+
     QVBoxLayout *mainLayout = new QVBoxLayout(mainWidget);
-    mainLayout->addWidget(homeButton, 0, Qt::AlignCenter);
+
+    //create the layout for the buttons
+    QHBoxLayout *buttonLayout = new QHBoxLayout();
+    buttonLayout->setSpacing(0);
+    buttonLayout->setContentsMargins(0, 0, 0, 0);
+    buttonLayout->addWidget(goalsButton);
+    buttonLayout->addWidget(habitsButton);
+    buttonLayout->addWidget(homeButton);
+    buttonLayout->addWidget(inspirationButton);
+    buttonLayout->addWidget(notesButton);
+
+    //create a spacer
+    QSpacerItem *spacer = new QSpacerItem(40, 200, QSizePolicy::Preferred, QSizePolicy::Minimum);
+    mainLayout->addItem(spacer);
+    mainLayout->addLayout(buttonLayout);
+    mainLayout->addItem(spacer);
 
     //button style
     QString buttonStyle = "QPushButton {"
@@ -54,7 +87,17 @@ MainWindow::MainWindow(QWidget *parent)
                           "QPushButton:hover {"
                           "background-color: #6C2DC7;"
                           "}";
+
     homeButton->setStyleSheet(buttonStyle);
+    goalsButton->setStyleSheet(buttonStyle);
+    habitsButton->setStyleSheet(buttonStyle);
+    inspirationButton->setStyleSheet(buttonStyle);
+    notesButton->setStyleSheet(buttonStyle);
+
+
+    //add the layout to the grid layout
+    gridLayout->addLayout(mainLayout, 0, 0, Qt::AlignTop | Qt::AlignHCenter);
+
 
     setCentralWidget(centralWidget);
 }
