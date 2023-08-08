01562f0988a12ec08b46d4ba75f4440092fa3ac2
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2023-08-08 23:05:48 +0100

    Update mainwindow.h

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -24,6 +24,7 @@ private slots:
     void resetWindow(QPushButton* inspirationButton,QPushButton *notesButton, QVBoxLayout *mainLayout, QHBoxLayout *buttonLayout, QLabel *helloLabel, QGridLayout *gridLayout, AspectRatioWidget *aspectRatioWidget);
     void on_inspirationButton_clicked(QPushButton* inspirationButton,QWidget* centralWidget, QWidget* mainWidget, QPushButton* notesButton, QVBoxLayout* mainLayout, QHBoxLayout* buttonLayout, QLabel* helloLabel, QGridLayout* gridLayout, AspectRatioWidget* aspectRatioWidget);
     void on_GoalsButton_clicked(QPushButton* inspirationButton,QPushButton* goalsButton,QWidget* centralWidget, QWidget* mainWidget, QPushButton* notesButton, QVBoxLayout* mainLayout, QHBoxLayout* buttonLayout, QLabel* helloLabel, QGridLayout* gridLayout, AspectRatioWidget* aspectRatioWidget);
+    void addGoal(int goalCounter, int totalGoalCounter, QWidget* goalsWidget, AspectRatioWidget* goalsAspectRatioWidget, QHBoxLayout* goalsContainerLayout);
     void addSubgoal(int subgoalIndex,int goalCounter,QString folderName,QVBoxLayout* subgoalsContainerLayout, QWidget* goalsWidget);
     void loadGoals(QPushButton* addGoalButton, int &goalCounter);
 private:
