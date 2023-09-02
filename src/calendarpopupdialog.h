751ad88ec457214b340b56ceea3e170d3a8eb3b9
ahmedkerouat <105589873+ahmedkerouat@users.noreply.github.com>
2023-09-02 16:40:00 +0100

    Update calendarpopupdialog.h

--- src/calendarpopupdialog.h
+++ src/calendarpopupdialog.h
@@ -2,7 +2,6 @@
 #include <QCalendarWidget>
 #include <QPushButton>
 #include <QVBoxLayout>
-#include <QToolBar>
 
 class CalendarPopupDialog : public QDialog {
     Q_OBJECT
@@ -22,14 +21,6 @@ public:
         QDate currentDate = QDate::currentDate();
            calendarWidget->setMinimumDate(QDate(currentDate.year(), 1, 1));
            calendarWidget->setMaximumDate(QDate(currentDate.year(), 12, 31));
-
-           QToolBar *navBar = calendarWidget->findChild<QToolBar*>("qt_calendar_navigationbar");
-           if (navBar) {
-               QWidget *yearButton = navBar->findChild<QWidget*>("qt_calendar_yearbutton");
-               if (yearButton) {
-                   yearButton->setVisible(false);
-               }
-           }
 
         connect(okButton, &QPushButton::clicked, this, &CalendarPopupDialog::accept);
     }
